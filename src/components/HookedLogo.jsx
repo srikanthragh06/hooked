56c9e956ced8eeabb49934825aed12b552e90e5f
sr1k5 <srikanthragh06@gmail.com>
2024-03-22 23:10:50 +0530

    Created HookedLogo component, added Hooked logo icon, and new Hooked font to index.css

---
+++ src/components/HookedLogo.jsx
@@ -0,0 +1,13 @@
+const HookedLogo = ({ className }) => {
+    return (
+        <div
+            className={`text-5xl text-yellow-600 relative 
+        bungee-shade-regular font-bold
+        px-2 py-1 ${className}`}
+        >
+            Hooked
+        </div>
+    );
+};
+
+export default HookedLogo;
