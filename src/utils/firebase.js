601cb2f0ab504736804cc7eb27af979a415d5e16
sr1k5 <srikanthragh06@gmail.com>
2024-03-24 00:55:02 +0530

    Removed consolelog from firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,7 +7,6 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-console.log(JSON.stringify(import.meta.env.VITE_API_KEY));
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_API_KEY,
     authDomain: import.meta.env.VITE_AUTH_DOMAIN,
